d9abeb01026df57b22fc55f9e3860a371ed34501
zevan rosser <bendvent@gmail.com>
2020-07-19 11:33:12 -0400

    solid 002

--- playground/002.js
+++ playground/002.js
@@ -3,23 +3,26 @@ smallDraw(({ canvas, c, resize, draw }) => {
     constructor({ size, scale, x, y }) {
       this.size = size || 6;
       this.cells = this.size ** 2;
-      this.scale = scale || 10;
+      this.scale = scale || 20;
       this.x = x;
       this.y = y;
       this.cycle = 0;
+      this.drawChance = 0.005;
       this.init();
     }
 
     init() {
       let detailChance = 0.2;
       this.locs = [];
+
       for (let i = 0; i < this.cells; i++) {
         let x = i % this.size;
         let y = Math.floor(i / this.size);
-        if (Math.random() < detailChance) {
+        if (Math.random() < detailChance || i === 0) {
           this.locs.push([this.x + x * this.scale, this.y + y * this.scale]);
         }
       }
+
       this.locs.sort(() => Math.random() - 0.5);
       this.step = 0;
       this.steps = this.locs.length;
@@ -32,36 +35,22 @@ smallDraw(({ canvas, c, resize, draw }) => {
         this.prevX = null;
         this.prevY = null;
       }
-
       this.cycle++;
       this.tick = Math.round(10 + Math.random() * 10);
-
-      this.s = 0;
-
-      for (let i = 0; i < this.steps; i++) {
-        let loc = this.locs[i];
-        c.fillStyle = 'rgba(255,255, 255, 0.4)';
-        c.strokeStyle = 'rgba(255,255, 255, 0.4)';
-        c.beginPath();
-        var r = 2 + ((i + Math.random() * 10) % 2); // Math.random() * 4;
-        c.arc(loc[0] * 0.9, loc[1] * 0.9, r, 0, Math.PI * 2, false);
-        c.fill();
-        c.stroke();
-      }
+      this.lineWidth = 1 + Math.random() * 3;
     }
 
-    draw() {
-      this.s += 0.1;
+    draw(inter, stroke) {
       this.life++;
 
       if (this.life % this.tick === 0) {
         this.step++;
         if (this.step >= this.steps) {
           this.x += 160;
-          if (this.x > window.innerWidth || Math.random() < 0.2) {
+          if (this.x > window.innerWidth + 100 || Math.random() < 0.2) {
             this.x = 40;
             this.y += 160;
-            if (this.y > window.innerHeight - 100) {
+            if (this.y > window.innerHeight + 200) {
               this.y = 80;
             }
             this.cycle = 0;
@@ -81,25 +70,37 @@ smallDraw(({ canvas, c, resize, draw }) => {
       this.by = ((this.py - this.dy) / 7 + this.by) / 1.2;
 
       if (this.prevX != null) {
-        c.strokeStyle = 'rgba(255, 255, 255, 0.9)';
-
-        c.beginPath();
-        c.moveTo(this.prevX, this.prevY);
-        c.lineTo(this.px, this.py);
-        c.stroke();
+        if (inter < this.drawChance || this.allow == true) {
+          this.allow = true;
+          setTimeout(() => {
+            this.allow = false;
+          }, Math.random() * 5000);
+          c.strokeStyle = stroke;
+          c.lineWidth = this.lineWidth;
+          c.beginPath();
+          c.moveTo(this.prevX, this.prevY);
+          c.lineTo(this.px, this.py);
+          c.stroke();
+        }
       }
       this.prevX = this.px;
       this.prevY = this.py;
     }
   }
 
-  let sym = new Sym({
-    x: 80,
-    y: 40
-  });
+  let syms = [];
+  let NUM = 30;
+  for (let i = 0; i < NUM; i++) {
+    syms.push(
+      new Sym({
+        x: 80,
+        y: 40
+      })
+    );
+  }
 
   const clear = () => {
-    c.fillStyle = 'black';
+    c.fillStyle = 'white';
     c.fillRect(0, 0, canvas.width, canvas.height);
   };
 
@@ -107,9 +108,15 @@ smallDraw(({ canvas, c, resize, draw }) => {
   clear();
 
   draw(() => {
-    //   c.scale(0.8, 0.8);
-    for (let i = 0; i < 5; i++) {
-      sym.draw();
+    c.save();
+    c.scale(0.65, 0.65);
+    for (let i = 0; i < NUM; i++) {
+      if (i === 0) {
+        syms[i].draw(0.001, 'rgba(0, 0, 0, 0.54)');
+      } else {
+        syms[i].draw(Math.random(), 'rgba(0, 0, 0, 0.14)');
+      }
     }
+    c.restore();
   });
 });
